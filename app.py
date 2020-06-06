aa7a1bb99431ec37d8a0b2f09580fe80fff32d0e
OriKedar <orikedarr@gmail.com>
2020-06-06 12:51:33 +0300

    New records and username and password

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def update(id):
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        if request.form['username'] != 'OriKedar' or request.form['password'] != 'records':
             error = 'Invalid Credentials. Please try again.'
         else:
             return redirect(url_for('index'))
